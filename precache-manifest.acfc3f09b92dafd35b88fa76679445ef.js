8271d68c7a4946685a9555ba54a1f1e412eda579
Matt Seidholz <mseidholz@gmail.com>
2020-03-05 11:10:19 -0500

    Updates

---
+++ precache-manifest.acfc3f09b92dafd35b88fa76679445ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "904905d04127cb4c5fc140d81fe598ab",
+    "url": "/nuggulator/index.html"
+  },
+  {
+    "revision": "223cab53ec8f30f22592",
+    "url": "/nuggulator/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "cd87d7503a9a21568313",
+    "url": "/nuggulator/static/css/main.32a9134e.chunk.css"
+  },
+  {
+    "revision": "223cab53ec8f30f22592",
+    "url": "/nuggulator/static/js/2.868afeda.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/nuggulator/static/js/2.868afeda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd87d7503a9a21568313",
+    "url": "/nuggulator/static/js/main.889941e1.chunk.js"
+  },
+  {
+    "revision": "7ab59eba2efad606eca9",
+    "url": "/nuggulator/static/js/runtime-main.bb95d33f.js"
+  }
+]);
